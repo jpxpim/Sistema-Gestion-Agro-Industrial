1265dbf219da880dd5acaec57fd778ac3ef8150f
jpxpim <jpxpim@gmail.com>
2014-07-21 15:14:28 -0500

    Sector: Entidad, DAO

---
+++ Sistema-Gestion-Agro-Industrial/src/java/Entidades/entSubSector.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entidades;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author rosemary
+ */
+public class entSubSector {
+    private int id_sub_sector;
+    private entSector objSector;
+    private String nombre;
+    private String descripcion;
+    private boolean estado;
+    private String usuario_responsable;
+    private Timestamp fecha_modificacion;
+    
+    public int getId_sub_sector() {
+        return id_sub_sector;
+    }
+
+    public void setId_sub_sector(int id_sub_sector) {
+        this.id_sub_sector = id_sub_sector;
+    }
+
+    public entSector getObjSector() {
+        return objSector;
+    }
+
+    public void setObjSector(entSector objSector) {
+        this.objSector = objSector;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getUsuario_responsable() {
+        return usuario_responsable;
+    }
+
+    public void setUsuario_responsable(String usuario_responsable) {
+        this.usuario_responsable = usuario_responsable;
+    }
+
+    public Timestamp getFecha_modificacion() {
+        return fecha_modificacion;
+    }
+
+    public void setFecha_modificacion(Timestamp fecha_modificacion) {
+        this.fecha_modificacion = fecha_modificacion;
+    }
+}
