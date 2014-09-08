d6159125d7bb47c0dfe797fd342152d4885c029a
jpxpim <jpxpim@gmail.com>
2014-09-08 17:22:05 -0500

    update
    update

---
+++ Sistema-Gestion-Agro-Industrial/src/java/Dao/ViaEmbarqueDAO.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Dao;
+
+import Entidades.entViaEmbarque;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author rosemary
+ */
+public class ViaEmbarqueDAO {
+    
+    public static List<entViaEmbarque> Listar(boolean activo) throws Exception
+    {
+        List<entViaEmbarque> lista = null;
+        Connection conn =null;
+        CallableStatement stmt = null;
+        ResultSet dr = null;
+        try {
+            String sql="select id_via_embarque,nombre,estado,usuario_responsable,fecha_modificacion"
+                    + " from via_embarque ";
+            if(activo)
+                        sql+=" where estado=1"; 
+
+            conn = ConexionDAO.getConnection();
+            stmt = conn.prepareCall(sql);
+            dr = stmt.executeQuery();
+
+            while(dr.next())
+            {
+                if(lista==null)
+                    lista= new ArrayList<entViaEmbarque>();
+                
+                    entViaEmbarque entidad = new entViaEmbarque();
+                    entidad.setId_via_embarque(dr.getInt(1));
+                    entidad.setNombre(dr.getString(2)); 
+                    entidad.setEstado(dr.getBoolean(3)); 
+                    entidad.setUsuario_responsable(dr.getString(4)); 
+                    entidad.setFecha_modificacion(dr.getTimestamp(5)); 
+                    lista.add(entidad);
+            }
+
+        } catch (Exception e) {
+            throw new Exception("Listar "+e.getMessage(), e);
+        }
+        finally{
+            try {
+                dr.close();
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+            }
+        }
+        return lista;
+    }
+    
+    public  static int insertar(entViaEmbarque entidad) throws Exception
+    {
+        int rpta = 0;
+        Connection conn =null;
+        PreparedStatement  stmt = null;
+        try {
+            
+           String sql="INSERT INTO via_embarque(nombre,estado,usuario_responsable,fecha_modificacion)"
+                   + " VALUES(?,?,?,GETDATE());";
+           
+            conn = ConexionDAO.getConnection();
+            stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, entidad.getNombre());
+            stmt.setBoolean(2, entidad.getEstado());
+            stmt.setString(3, entidad.getUsuario_responsable());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            
+            if (rs.next()){
+                rpta=rs.getInt(1);
+            }
+            rs.close();
+        } catch (Exception e) {
+            throw new Exception("Insertar"+e.getMessage(), e);
+        }
+        finally{
+            try {
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+            }
+        }
+        return rpta;
+    } 
+    
+    public static boolean actualizar(entViaEmbarque entidad) throws Exception
+    {
+        boolean rpta = false;
+        Connection conn =null;
+        CallableStatement stmt = null;
+        try {
+             String sql="UPDATE cultivo SET nombre = ?,estado= ?,"
+                     + "usuario_responsable = ?,fecha_modificacion = GETDATE() WHERE id_via_embarque = ?;";
+             
+            conn = ConexionDAO.getConnection();
+            stmt = conn.prepareCall(sql);             
+            stmt.setString(1, entidad.getNombre());
+            stmt.setBoolean(2, entidad.getEstado());
+            stmt.setString(3, entidad.getUsuario_responsable());
+            stmt.setInt(4,entidad.getId_via_embarque());
+                
+           rpta = stmt.executeUpdate() == 1;
+        } catch (Exception e) {
+            throw new Exception("Error Actualizar "+e.getMessage(), e);
+        }
+        finally{
+            try {
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+            }
+        }
+        return rpta;
+    }
+}
---
+++ Sistema-Gestion-Agro-Industrial/src/java/Entidades/entViaEmbarque.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author rosemary
+ */
+public class entViaEmbarque {
+    
+    private int id_via_embarque;
+    private String nombre;
+    private String usuario_responsable;
+    private Date fecha_modificacion;
+    private Boolean estado;
+
+    public entViaEmbarque (){}
+    public int getId_via_embarque() {
+        return id_via_embarque;
+    }
+
+    public void setId_via_embarque(int id_via_embarque) {
+        this.id_via_embarque = id_via_embarque;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario_responsable() {
+        return usuario_responsable;
+    }
+
+    public void setUsuario_responsable(String usuario_responsable) {
+        this.usuario_responsable = usuario_responsable;
+    }
+
+    public Date getFecha_modificacion() {
+        return fecha_modificacion;
+    }
+
+    public void setFecha_modificacion(Date fecha_modificacion) {
+        this.fecha_modificacion = fecha_modificacion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
